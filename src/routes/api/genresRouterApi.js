ed5e8ad0adbe3154aa759b1b7c0f89a9fbbb56c2
Maximo <maxiarebalo12@gmail.com>
2022-01-03 20:18:41 -0300

    first commit

---
+++ src/routes/api/genresRouterApi.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const genresAPIController = require('../../controllers/api/genresController');
+
+router.get('/', genresAPIController.list);
+router.get('/:id', genresAPIController.detail);
+
+module.exports = router;
